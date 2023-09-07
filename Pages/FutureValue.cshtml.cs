bb5fa5fc8621b2c82c7176ca6352c79197583017
DESKTOP-RJMDTP3\owner <Jonathanliu865@gmail.com>
2023-09-07 13:37:01 -0700

    first commit

---
+++ Pages/FutureValue.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorApp.Models;
+
+namespace RazorApp.Pages
+{
+    public class FutureValueModel : PageModel
+    {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+        public void OnGet()
+        {
+            ViewData["FV"] = 0;
+        }
+        public void OnPost(FutureValue model)
+        {
+            ViewData["FV"] = model.CalculateFutureValue();
+        }
+    }
+}
